c65a340a26619d4391a4c6bd27252979b0cd2453
leannephillipz <leanne.j.phillips@gmail.com>
2021-02-10 15:06:52 +0000

    seperate admin in nav

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,13 +1,16 @@
 <template >
     <transition name="fade">
         <nav>
-          <ul class="nav">
+          <ul>
           <li><router-link to="/">Home</router-link></li>
           <li><router-link to="/grade">Grade</router-link></li>
-          <li><router-link to="/addstudent">Add Student</router-link></li>
           <li><router-link :to="{ name: 'students'}">Students</router-link></li>
-          <li><router-link to="/addcourse">Add Course</router-link></li>
           <li><router-link to="/courses">Courses</router-link></li>
+        </ul>
+        <hr>
+        <ul>
+          <li><router-link to="/addstudent">Add Student</router-link></li>
+          <li><router-link to="/addcourse">Add Course</router-link></li>
         </ul>
         </nav>
     </transition>
