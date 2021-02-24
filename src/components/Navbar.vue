0b28187f92ccc0a8aecf854d39f686dee52924a2
leannephillipz <leanne.j.phillips@gmail.com>
2021-02-24 13:35:59 +0000

    menu toggle

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,51 @@
 <template>
-  <div v-if="user">
-    <nav>
-      <router-link :to="{ name: 'Home' }">Home</router-link>
-      <router-link :to="{ name: 'Students' }">Students</router-link>
-      <router-link :to="{ name: 'Courses' }">Courses</router-link>
-      <router-link :to="{ name: 'Tasks' }">Tasks</router-link>
-      <hr>
-      <router-link :to="{ name: 'AddTask' }">Create Task</router-link>
-      <router-link :to="{ name: 'AddCourse' }">Create Course</router-link>
-      <router-link :to="{ name: 'AddStudent' }">Create Student</router-link>
 
+    <nav :class="shownav ? 'navtoggle' : ''" v-if="user">
+      <div class="links">
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+        <router-link :to="{ name: 'Students' }">Students</router-link>
+        <router-link :to="{ name: 'Courses' }">Courses</router-link>
+        <router-link :to="{ name: 'Tasks' }">Tasks</router-link>
+        <hr>
+        <router-link :to="{ name: 'AddTask' }">Create Task</router-link>
+        <router-link :to="{ name: 'AddCourse' }">Create Course</router-link>
+        <router-link :to="{ name: 'AddStudent' }">Create Student</router-link>
+      </div>
+      <div class="icon">
+        <img v-if="shownav"  @click="toggle" src="../assets/icons/chevron_right-24px.svg" alt="">
+        <img v-if="!shownav"  @click="toggle" src="../assets/icons/chevron_left-24px.svg" alt="">
+      </div>
     </nav>
-    </div>
+
 </template>
 
 <script>
+import { ref } from 'vue'
 import getUser from '@/composables/getUser'
 export default {
 setup() {
   const { user } = getUser()
-  return { user }
+  const shownav = ref(false);
+  const toggle =  () => {
+
+    // !shownav.value ? shownav.value == false : shownav.value == true //ternary
+
+    console.log(shownav.value)
+    if(shownav.value == true) {
+      shownav.value = false
+    } else if(shownav.value == false)
+    {
+      shownav.value  = true
+    }
+  }
+
+  return { user, toggle, shownav}
+
+
 }
 }
 </script>
 
-<style>
+<style >
 
 </style>
