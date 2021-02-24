873a16116e3a9304a7a4b83a4362ad15b2c1dcab
leannephillipz <leanne.j.phillips@gmail.com>
2021-02-24 12:09:55 +0000

    style

--- src/views/courses.vue
+++ src/views/courses.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="">
-    <h1>Courses</h1>
-      <div v-if="error">{{ error }}</div>
-      <div v-for="course in courses" class='item' :key="course.id">
+  <h1>Courses</h1>
+  <div v-if="error">{{ error }}</div>
+  <div class="courses flex">
 
+      <div v-for="course in courses" class='item' :key="course.id">
         <router-link :to="{ name: 'Course', params: { code: course.code  }}">
-        <p class="name">{{ course.title }} - Level {{ course.lvl }} - year {{ course.year }}</p>
+          <h3>{{ course.title }} Level {{ course.lvl }}</h3>
+          <p>year {{ course.year }}</p>
         </router-link>
+
       </div>
   </div>
 
@@ -29,7 +31,36 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+
+.item {
+  display: inline-block;
+    background: #566e7a;
+    border-radius: 0.3em;
+    padding: 1em;
+    margin-right: 1em;
+    text-transform: capitalize;
+    flex: 1 1 50%;
+  }
+  h3 {
+    font-size: 1.3rem;
+    border-bottom: 1px solid #252a2f;
+    padding-bottom: 0.5em;
+    margin-bottom: 0.5em;
+  }
+.item:hover {
+    background: #718a96;
+  }
+
+  p {
+    margin: 0;
+  }
+
+.item a {color:#fff;}
+.item a:hover {
+  text-decoration: none;
+}
 
 
 </style>
