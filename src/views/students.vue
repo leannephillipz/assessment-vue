a9fc52e248bf4e000baafb70fbf0635b099a3d62
Leanne <30321648+leannephillipz@users.noreply.github.com>
2021-04-14 17:05:11 +0100

    trying to sort array of objects

--- src/views/students.vue
+++ src/views/students.vue
@@ -14,14 +14,15 @@
             <li></li>
           </ul>
         </li>
-      <li v-for="student in students" class='item' :key="student.id">
+      <li v-for="student in sortedStudents" class='item' :key="student.id">
         <router-link :to="{ name: 'Student', params: { slug: student.id  }}">
           <ul class="flex ">
             <li >
               <span v-if="student.flags" v-for="flag in student.flags" :key="flag">*</span></li>
             <li><span v-if="student.aka">({{ student.aka }}) </span>{{ student.fname }} </li>
             <li>{{ student.lname }}</li>
-            <li>{{ student.coursecode }}</li>
+            <!-- <li>{{ student.coursecode }}</li> -->
+              <li>{{ student.group }}</li>
             <li><span v-if="student.status" class="pill" :class="student.status">{{ student.status }}</span></li>
             <li></li>
           </ul>
@@ -41,7 +42,7 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import getCollection from '@/composables/getCollection'
 
 export default {
@@ -51,9 +52,38 @@ export default {
     const { content:students, error, collect } = getCollection()
 
     collect('students')
+    // const sortedStudents = students.sort(
+    //   function(a, b){return a.group - b.group}
+    // )
+  //
+  //   function sortedStudents(...students){
+  //    return students.sort(function(a,b){
+  //      return a.group - b.group;
+  //    });
+  // }
 
-    return { students, error }
+    // const sortedStudents = students.group.sort(
+    //   function(a, b){return a.group - b.group}
+    // )
+
+    //can't sort
+      const sortedStudents = students
+    // const sortedStudents = computed((...students) => {
+    //   return students.sort(
+    //     function(a, b){return a - b}
+    //   )
+    // })
+
+    return { students, error, sortedStudents }
   },
+  // computed: {
+  //   sortedStudents(...students){
+  //      return students.sort(function(a,b){
+  //        return a.group - b.group;
+  //      })
+  //   }
+  //
+  // }
 }
 </script>
 
